a7ab670577b4a3e77770fc28819228d3203ca76f
Harounkane <165789938+Harounkane@users.noreply.github.com>
2024-06-03 11:03:04 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -7,10 +7,6 @@ import sqlite3
                                                                                                                                        
 app = Flask(__name__)
 
-@app.route("/rapport/")
-def mongraphique():
-    return render_template("graphique.html")
-  
 @app.route('/histogramme/')
 def meteo():
     response = urlopen('https://samples.openweathermap.org/data/2.5/forecast?lat=0&lon=0&appid=xxx')
@@ -22,6 +18,10 @@ def meteo():
         temp_day_value = list_element.get('main', {}).get('temp') - 273.15 # Conversion de Kelvin en °c 
         results.append({'Jour': dt_value, 'temp': temp_day_value})
     return jsonify(results=results)
+  
+@app.route("/rapport/")
+def mongraphique():
+    return render_template("graphique.html")
   
 @app.route('/tawarano/')
 def meteo():
